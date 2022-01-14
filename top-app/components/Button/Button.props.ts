a937b2273dd42dfcbded00baece6c5f29268f092
Aleksandr Ermolov <90044699+AleksandrErmolov@users.noreply.github.com>
2022-01-14 17:51:23 +0300

    add button

---
+++ top-app/components/Button/Button.props.ts
@@ -0,0 +1,8 @@
+import { ButtonHTMLAttributes, ReactNode, DetailedHTMLProps } from 'react';
+
+export interface ButtonProps extends DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> { 
+    children: ReactNode;
+    appearance: 'primary' | 'ghost';
+    arrow?: 'right' | 'down' | 'none';
+
+}
--- top-app/pages/index.tsx
+++ top-app/pages/index.tsx
@@ -1,9 +1,14 @@
 import Image from 'next/image';
+import Htag from '../components/Htag/Htag';
+import Button from '../components/Button/Button';
 
 export default function Home():JSX.Element {
   return (
     <div>
-      fdsfsdf
+      <Htag tag='h1'> Текст </Htag>
+      <Button appearance='primary' arrow='right'> Кнопка </Button>
+      <Button appearance='ghost' > Кнопка </Button>
+
     </div>
   );
 }
