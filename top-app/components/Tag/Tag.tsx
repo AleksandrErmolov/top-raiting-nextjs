2232a001487a38d861e416bfe3f6771c85bab179
Aleksandr Ermolov <90044699+AleksandrErmolov@users.noreply.github.com>
2022-01-15 14:40:56 +0300

    add Tag

---
+++ top-app/components/Tag/Tag.tsx
@@ -0,0 +1,35 @@
+import { TagProps } from './Tag.props'
+import styles from './Tag.module.css'
+import cn from 'classnames'
+
+
+export const Tag = ({ size = 's', children, color = 'ghost', className, href, ...props }: TagProps): JSX.Element => {
+
+
+    return (
+
+
+        <div
+            className={cn(styles.tag, {
+                [styles.s]: size == 's',
+                [styles.m]: size == 'm',
+                [styles.ghost]: color == 'ghost',
+                [styles.red]: color == 'red',
+                [styles.grey]: color == 'grey',
+                [styles.green]: color == 'green',
+                [styles.primary]: color == 'primary',
+
+            })}
+
+            {...props}
+        >
+            { href
+                ? <a href={href}>{children}</a>
+                : <>{children}</>
+            }
+
+
+        </div>
+
+    )
+}
--- top-app/pages/index.tsx
+++ top-app/pages/index.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { Button, Htag, P } from '../components/';
+import { Button, Htag, P, Tag } from '../components/';
 
 
 export default function Home(): JSX.Element {
@@ -11,6 +11,13 @@ export default function Home(): JSX.Element {
       <P size='s'> dfsfsf </P>
       <P> dfsfsf </P>
       <P size='l'> dfsfsf </P>
+      <Tag size='s'>Маленький</Tag>
+      <Tag size='m' color='red'>Red</Tag>
+      <Tag size='s' color='green'>Зелёный</Tag>
+      <Tag color="primary">Маленький</Tag>
+
+
+
     </div>
   );
 }
